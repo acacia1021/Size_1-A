e9346db0ba8bf9915d29bb35e6aba6bba8f58616
acacia1021 <acacia.lord@faulkner.edu>
2019-04-25 09:33:51 -0500

    heheh

--- app/src/main/java/com/example/size1/HomeScreen.java
+++
@@ -1,4 +0,0 @@
-package com.example.size1;
-
-public class HomeScreen {
-}
--- app/src/main/java/com/example/size1/fragments/MainActivity.java
+++ app/src/main/java/com/example/size1/fragments/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.size1;
+package com.example.size1.fragments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.size1.R;
 
 public class MainActivity extends AppCompatActivity {
 
